f2b81a39bde3fb6b270d396fba51b65334dcd7cd
sameer singh <sameerbisht2315@gmail.com>
2026-03-21 22:52:32 +0530

    Added the boilerPlate code for the FakeCommerceApp

---
+++ src/main/java/com/example/FakeCommerceApp/FakeCommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FakeCommerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeCommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeCommerceAppApplication.class, args);
+	}
+
+}
