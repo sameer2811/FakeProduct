9772175ef18afb3bccaa2f35bf797a1eb2d3095c
sameer singh <sameerbisht2315@gmail.com>
2026-03-23 00:35:42 +0530

    Pushing the changes

--- src/main/java/com/example/FakeCommerceApp/schema/Product.java
+++ src/main/java/com/example/FakeCommerceApp/schema/Product.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 
 @Entity
 @Data
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product extends BaseEntity{
